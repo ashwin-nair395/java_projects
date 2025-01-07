f682f6275d25f0729fd2bf23ebe7e8879d40b3da
Ashwin Nair <ashwinbox560@gmail.com>
2025-01-07 14:34:26 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,19 @@
+package com.pcu.javapro;
+
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a, b, c, d, e, f;
+        a=10;
+        b=20;
+        c=a+b;
+        d=b-a;
+        e=a*b;
+        f=b/a;
+        System.out.println("Addition: "+c);
+        System.out.println("Subtraction: "+d);
+        System.out.println("Multiplication: "+e);
+        System.out.println("Division: " +f);
+
+    }
+
+}
---
+++ VariDeclare.java
@@ -0,0 +1,11 @@
+package com.pcu.javapro;
+
+public class VariDeclare {
+        static String a = "PCU";
+        int b =2;
+
+    public static void main(String[] args) {
+        int c =20;
+        System.out.println("Value is "+c+", College name"+a);
+    }
+}
